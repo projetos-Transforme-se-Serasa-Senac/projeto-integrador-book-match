7dcb865df180ff24d8cac8ac2da7c19a80bcd7b9
Giulia Maia <maiagiulia10@gmail.com>
2022-05-26 16:47:59 -0300

    organização de componentes

---
+++ src/componets/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Menu = () => {
+    return ( 
+        <div>
+
+        <div>
+            <img scr="#"/>
+        </div>
+
+
+        </div>
+     );
+}
+ 
+export default Menu;
