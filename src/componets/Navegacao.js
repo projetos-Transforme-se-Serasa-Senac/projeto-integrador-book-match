01eb287b031a123c06f49317b9865fa1aa17a75d
Giulia Maia <maiagiulia10@gmail.com>
2022-05-31 15:02:17 -0300

    inserção dos links da navegação

--- src/componets/Navegacao.js
+++ src/componets/Navegacao.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import { Link } from "react-router-dom";
+
+
 const Navegacao = () => {
     return ( 
         <div>
 
-
+            <Link to="/match"> Match </Link>  
+            | 
+            <Link to="/feed"> Feed </Link>
+            |
+            <Link to="/comunidade"> Comunidade </Link>
+            | 
+            <Link to="/chat"> Chat </Link>
             
         </div>
      );
