4aaa7293e05baedc834329c3d9ec86a3ec49ee71
Giulia Maia <maiagiulia10@gmail.com>
2022-05-31 15:00:52 -0300

    importação dos tópicos da navegação  e criação das rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
+import Menu from './componets/Menu';
+
+import Navegacao from './componets/Navegacao';
+
+import { Routes, Route } from "react-router-dom"; 
+
+import Match from './Match';
+
+import Feed from './Feed';
+
+import Comunidade from './Comunidade';
+
+import Chat from './Chat';
+
+
 
 const App = () => {
   return ( 
     <div>
 
+    <Navegacao/> 
+
+    <Routes>
+      <Route path="/match" element={<Match/>}/>
+      <Route path="/feed" element={<Feed/>}/>
+      <Route path="/comunidade" element={<Comunidade/>}/>
+      <Route path="/chat" element={<Chat/>}/>
+    </Routes>
 
 
     </div>
