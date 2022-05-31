13deded8f2bdec4eba5169a3f7f1cfc1b06ec92d
Giulia Maia <maiagiulia10@gmail.com>
2022-05-31 16:36:45 -0300

    Update Menu.js

--- src/componets/Menu.js
+++ src/componets/Menu.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
+import { Link } from "react-router-dom";
+
 const Menu = () => {
     return ( 
         <div>
+                {/* <div>
+                    <img scr="#" LOGO/>
+                </div> */}
 
-        <div>
-            <img scr="#"/>
-        </div>
+            <div>
+
+                <Link to="/"> Cadastro Livros </Link>
+                <Link to="/"> Perfil </Link>
+                <Link to="/"> Emprestados </Link>
+
+            </div>
 
 
         </div>
@@ -14,3 +23,6 @@ const Menu = () => {
 }
  
 export default Menu;
+
+// cadastrar livros, seu perfil (acessar o perfil) ,
+ // Empréstimos (ver os livros que ela emprestou)
