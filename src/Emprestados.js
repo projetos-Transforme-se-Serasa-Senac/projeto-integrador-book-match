2d235364be7da23c47e37598568e4111c926b37d
Giulia Maia <maiagiulia10@gmail.com>
2022-06-02 16:24:57 -0300

    criação da página para ver os livros emprestados

---
+++ src/Emprestados.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Emprestados = () => {
+    return ( 
+        <div>
+
+            <h1> Livros Emprestados </h1>
+
+        </div>
+     );
+}
+ 
+export default Emprestados;
