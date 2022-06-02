004a3a620b812f44900c7cf3b4bf69ac7d554e42
aliceSCosta <51675554+aliceSCosta@users.noreply.github.com>
2022-06-02 16:26:04 -0300

    Adicionando o elemento de rotas no APP.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
 import React from 'react';
+// import { Routes, Route } from "react-router-dom"; 
 
-import Menu from './components/Menu';
-
-import Navegacao from './components/Navegacao';
-
-import { Routes, Route } from "react-router-dom"; 
-
-import Match from './Match';
-
-import Feed from './Feed';
-
-import Comunidade from './Comunidade';
-
-import Chat from './Chat';
+// import Menu from './components/Menu';
+// import Navegacao from './components/Navegacao';
+// import Match from './Match';
+// import Feed from './Feed';
+// import Comunidade from './Comunidade';
+// import Chat from './Chat';
+// import SignUp from './SignUp';
+import AppRouter from './routes';
 
 
 
 const App = () => {
   return ( 
-    <div>
+    <main>
 
-    <Menu/>
+      <AppRouter/>
 
-    <Navegacao/> 
+      {/* <Menu/>
 
-    <Routes>
-      <Route path="/match" element={<Match/>}/>
-      <Route path="/feed" element={<Feed/>}/>
-      <Route path="/comunidade" element={<Comunidade/>}/>
-      <Route path="/chat" element={<Chat/>}/>
-    </Routes>
+      <Navegacao/> 
 
+      <Routes>
+        <Route path="/match" element={<Match/>}/>
+        <Route path="/feed" element={<Feed/>}/>
+        <Route path="/comunidade" element={<Comunidade/>}/>
+        <Route path="/chat" element={<Chat/>}/>
+      </Routes>
 
-    </div>
-   );
+      <SignUp/> */}
+
+    </main>
+  );
 }
  
 export default App;
