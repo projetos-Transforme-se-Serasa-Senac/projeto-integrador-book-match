3a440fc2357ed4386a2356c8e9862efb3ba13335
Giulia Maia <maiagiulia10@gmail.com>
2022-06-02 16:42:38 -0300

    coisas que precisam ter no cadastramento de livros

--- src/CadastroLivros.js
+++ src/CadastroLivros.js
@@ -7,8 +7,28 @@ const CadastroLivros = () => {
 
             <h1> Cadastro de Livros </h1>
 
+
+            <button> Cadastrar </button>
+            <br/>
+            <br/>
+            <button> Cancelar </button>
+
         </div>
      );
 }
  
 export default CadastroLivros;
+
+// espaço para foto 
+
+// nome do livro
+// autor
+// gênero
+// classificação etária
+// tags
+// data da devolução
+// aluguel (como opcional)
+// sinopse
+
+// botão cadastrar 
+// botão cancelar
