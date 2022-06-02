0145a97379851221fdaa51afbd55908681f5e561
IsabelaCorreia12 <104646467+IsabelaCorreia12@users.noreply.github.com>
2022-06-02 14:12:22 -0300

    mudança no menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,21 +8,22 @@ const Menu = () => {
 
     return ( 
         <header>
-            <Link to="/" className="logo"><img src="https://i.imgur.com/QcRjnUP.png" /></Link>
+            <Link to="/" ><img className="logo" src="https://i.imgur.com/QcRjnUP.png" /></Link>
 
             <div className="pesquisar" >
                 <input type="text"/><img className="lupa" src="https://i.imgur.com/rFe6FMK.png"/>
             </div>
 
-            <div>
-
-                <Link to="/"> Cadastro Livros </Link>
-                <Link to="/"> Perfil </Link>
-                <Link to="/"> Emprestados </Link>
-
-            </div>
 
             <a href="#"> <img class="perfil" src="https://i.imgur.com/o8Mx6B5.jpg"/></a>
+
+            <div className='opcoesMenu'>
+                <ul>
+                    <li><Link to="/"> Cadastro Livros </Link></li>
+                    <li><Link to="/"> Perfil </Link></li>
+                    <li><Link to="/"> Emprestados </Link></li>
+                </ul>             
+            </div>
 
 
         </header>
