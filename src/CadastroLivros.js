1ebc6bae3450e25d0b08de8ba839c4447339e7fb
Giulia Maia <maiagiulia10@gmail.com>
2022-06-02 16:24:38 -0300

    criação da página para cadastrar livros

---
+++ src/CadastroLivros.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const CadastroLivros = () => {
+    return ( 
+        <div>
+
+            <h1> Cadastro de Livros </h1>
+
+        </div>
+     );
+}
+ 
+export default CadastroLivros;
