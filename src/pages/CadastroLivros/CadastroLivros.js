a400a336d69d6eafaefd0e496ccc7aa20d9c1620
Giulia Maia <maiagiulia10@gmail.com>
2022-06-03 16:39:19 -0300

    cadastro de livros

--- src/pages/CadastroLivros/CadastroLivros.js
+++ src/pages/CadastroLivros/CadastroLivros.js
@@ -8,28 +8,37 @@ const CadastroLivros = () => {
                 <h1> Cadastro de Livros </h1>
 
                 <label for='nome'> Nome do Livro </label>
-                <input id="nome" type='text'/>
+                <input id="nome" type='text' required/>
 
-                <label for='nome'> Autor </label>
-                <input id="nome" type='text'/>
+                <label for='autor'> Autor </label>
+                <input id="autor" type='text' required/>
 
-                <label for='nome'> Gênero </label>
-                <input id="nome" type='text'/>
+                <label for='genero'> Gênero </label>
+                <input id="genero" type='text'required/>
 
-                <label for='nome'> Classificação Etária  </label>
-                <input id="nome" type='text'/>
+                <label for='classificacao' > Classificação Indicativa  </label>
+                <select id='classificacao' required>
+                    <option value="0"> -- </option> 
+                    <option value="1"> L </option>
+                    <option value="2"> 10 </option>
+                    <option value="3"> 12 </option>
+                    <option value="4"> 14 </option>
+                    <option value="5"> 16 </option>
+                    <option value="6"> 18+ </option>
+                </select>
+                
+                <label for='tags'> Tags </label>
+                <input id="tags" type='text'/>
 
-                <label for='nome'> Tags </label>
-                <input id="nome" type='text'/>
+                <label for='devolucao' required> Data da Devolução </label>
+                <input id="devolucao" type='date'/>
 
-                <label for='nome'> Data da Devolução </label>
-                <input id="nome" type='text'/>
+                <label for='aluguel'> Aluguel (opcional)</label>
+                <input id="aluguel" type='number'/>
 
-                <label for='nome'> Aluguel (opcional)</label>
-                <input id="nome" type='text'/>
 
-                <label for='nome'> Sinopse </label>
-                <input id="nome" type='text'/>
+                <label for='sinopse'> Sinopse </label>
+                <textarea id='sinopse' rows='5' cols='35' required> </textarea>
 
                 <input type='submit' value='Cadastrar'/>
                 <input type='submit' value='Cancelar'/>
@@ -40,17 +49,3 @@ const CadastroLivros = () => {
 }
  
 export default CadastroLivros;
-
-// espaço para foto 
-
-// nome do livro
-// autor
-// gênero
-// classificação etária
-// tags
-// data da devolução
-// aluguel (como opcional)
-// sinopse
-
-// botão cadastrar 
-// botão cancelar
