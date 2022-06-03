8955f132387a0e74959c60ac93b7a4e5cfbcbbba
IsabelaCorreia12 <104646467+IsabelaCorreia12@users.noreply.github.com>
2022-06-03 16:42:37 -0300

    estilizando o login

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,22 +1,40 @@
+import { useState } from "react";
 
 
 const SignIn = () => {
-    
+     require ('./SignIn.css')
+
+    const [usuario, setUsuario] = useState ("")
     
     return(
-        <div>
+        <div className="container-login">
             {/* {document.getElementById('menu').innerHTML='jasjdaj'} */}
-            <div>
-                <form>
+            <div class="wrapLogin">
+                <form className="loginForm">
+                    <span className="login-form-title">Bem vindo!</span>
 
-                    <label for='usuario'> Usuário </label>
-                    <input id="usuario" type='text'/>
+                    <span className="login-form-title">
+                        <img src="https://i.imgur.com/P3vDZ0n.jpg"/>
+                    </span>
+                    <div className="wrapInput">
+                        <input id="usuario" type='text'/>
+                        <span className="focusInput" data-placeholder="Usúario"></span>
+                      
+                    </div>
+                    <div className="wrapInput">
+                        <input id="senha" type='password'/>
+                        <span className="focusInput" data-placeholder="Usúario"></span>
 
-                    <label for='senha'> Senha </label>
-                    <input id="senha" type='password'/>
+                        <input type='submit' value='Entrar'/>
+                    </div>
+                    <div className="conteiner-login-form-btn">
+                        <button className="login-form-btn"></button>
+                    </div>
 
-                    <input type='submit' value='Entrar'/>
-
+                    <div className="text-center">
+                        <span className="txt1">Não possui conta?</span>
+                        <a className="txt2" href="#">Criar conta</a>
+                    </div>
                 </form>
             </div>
             <div>
