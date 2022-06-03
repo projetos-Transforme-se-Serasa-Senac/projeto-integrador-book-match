b4c00855ae60cf9a0deda0df3810d485f4ad8a87
aliceSCosta <51675554+aliceSCosta@users.noreply.github.com>
2022-06-03 16:40:19 -0300

    Colocando required nos inputs

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -3,19 +3,19 @@ const SignUp = () => {
         <section>
             <form method="POST">
                 <label for='nome'> Nome </label>
-                <input id="nome" type='text'/>
+                <input id="nome" type='text' required/>
 
                 <label for='usuario'> Usuário </label>
-                <input id="usuario" type='text'/>
+                <input id="usuario" type='text' required/>
 
                 <label for='email'> Email </label>
-                <input id="email" type='email'/>
+                <input id="email" type='email' required/>
 
                 <label for='senha'> Senha </label>
-                <input id='senha' type='password'/>
+                <input id='senha' type='password' required/>
 
                 <label for='confirmaSenha'> Confirme a Senha </label>
-                <input id="confirmaSenha" type='password'/>
+                <input id="confirmaSenha" type='password' required/>
 
                 <input type='submit' value='Cadastrar'/>
             </form>
