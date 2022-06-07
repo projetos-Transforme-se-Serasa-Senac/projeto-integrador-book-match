547f29df5c9b3fa5b6d6020aa4d39bd5caa94f43
Giulia Maia <maiagiulia10@gmail.com>
2022-06-07 16:49:28 -0300

    livros emprestados

--- src/pages/Emprestados/Emprestados.js
+++ src/pages/Emprestados/Emprestados.js
@@ -4,6 +4,33 @@ import Menu from '../../components/Menu/Menu';
 import Navegacao from '../../components/Navegacao/Navegacao';
 
 const Emprestados = () => {
+
+    const livros = [
+        {
+           nome: "Livro: A menina que roubava livros",
+           autor: "Autor: Markus Zusak",
+           devolucao: "Data de devolução: 07/05",
+           pessoa: "Emprestou: Giulia",
+           imagem: "https://i.imgur.com/xMs70Vl.png"
+        },
+
+        {
+            nome: "O assassinato no expresso oriente",
+            devolucao: "Data de devolução: 07/05",
+            autor: "Autor: Agatha Christie",
+            pessoa: "Emprestou: Giulia",
+            imagem: "https://i.imgur.com/2E3nakQ.png" 
+         },
+
+         {
+            nome: "Corte de espinhos e rosas",
+            devolucao: "Data de devolução: 07/05",
+            autor: "Autor: Sarah J. Maas",
+            pessoa: "Emprestou: Giulia",
+            imagem: "https://i.imgur.com/im9zDIr.jpg" 
+         }
+    ]
+
     return ( 
         <div>
 
@@ -11,6 +38,27 @@ const Emprestados = () => {
             <Navegacao/>
 
             <h1> Livros Emprestados </h1>
+            <br/>
+            <br/>
+
+            {
+                livros.map( livros => {
+                    return(
+
+                        <div className = "livros">
+                            <h3> { livros.nome } </h3>
+                            <h5> {livros.autor} </h5>
+                            <h5> {livros.pessoa} </h5>
+                            <h5> {livros.devolucao} </h5>
+                            <img src = {livros.imagem}  width = "250px"/>
+                            <br/>
+                            <br/>
+                            <br/>
+                        </div>
+                    )                 
+                })
+            }
+
 
         </div>
      );
