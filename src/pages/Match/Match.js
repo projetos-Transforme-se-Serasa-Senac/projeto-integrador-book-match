8497ad6bc34cb899843df3cc6ae089e79980c2f6
Giulia Maia <maiagiulia10@gmail.com>
2022-06-13 16:13:08 -0300

    Update Match.js

--- src/pages/Match/Match.js
+++ src/pages/Match/Match.js
@@ -4,26 +4,64 @@ import Menu from '../../components/Menu/Menu';
 import Navegacao from '../../components/Navegacao/Navegacao';
 
 const Match = () => {
+
+    const match = [
+        {
+           imagem: "https://i.imgur.com/6g4q4gh.png",
+           nome: "A menina que roubava livros",
+           botaoVoltar: "Voltar",
+           botaoNaoCurtir: " Não Curtir",
+           botaoCurtir: "Curtir",
+           botaoSeguir: "Seguir",
+        },
+
+        {
+            imagem: "https://imgur.com/K8rWLXD.png",
+            nome: "O assassinato no expresso oriente",
+            botaoVoltar: "Voltar",
+            botaoNaoCurtir: " Não Curtir",
+            botaoCurtir: "Curtir",
+            botaoSeguir: "Seguir",
+         },
+
+         {
+            imagem: "https://imgur.com/gIH07su.png",
+            nome: "Corte de espinhos e rosas",
+            botaoVoltar: "Voltar",
+            botaoNaoCurtir: "Não Curtir",
+            botaoCurtir: "Curtir",
+            botaoSeguir: "Seguir",
+         }
+    ]
+
+
     return ( 
         <div>
             <Menu/>
             <Navegacao/>
             <h1> Match </h1>
 
-            <div>
-                <img src='https://i.imgur.com/6g4q4gh.png' width="250px"/> 
-                <p> A menina que roubava livros </p>
-            </div>
+            {
+                match.map( match => {
+                    return(
 
-            <br/>
-            <br/>
-
-            <div>
-                <button className='voltar'> Voltar </button>
-                <button className='nãoCurtir'> Não curtir </button>
-                <button data-testid="gamepadLike"className='curtir'> Curtir </button>
-                <button className='seguir'> Seguir </button>
-            </div>
+                        <ul className = "match">
+                            <li>
+                                <img src= {match.imagem} width="200px" />
+                                <h3> {match.nome} </h3>
+                                <button> {match.botaoVoltar} </button>
+                                <button> {match.botaoNaoCurtir} </button>
+                                <button> {match.botaoCurtir} </button>
+                                <button> {match.botaoSeguir} </button>
+                                
+                                <br/>
+                                <br/>
+                                <br/>
+                            </li>
+                        </ul>
+                    )                 
+                })
+            }
 
         </div>
      );
