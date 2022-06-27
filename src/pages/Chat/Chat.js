dfaa91ed4aff5f332af1208680d1959215f9e791
IsabelaCorreia12 <104646467+IsabelaCorreia12@users.noreply.github.com>
2022-06-27 14:55:24 -0300

    criação do chat

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -4,12 +4,46 @@ import Menu from '../../components/Menu/Menu';
 import Navegacao from '../../components/Navegacao/Navegacao';
 
 const Chat = () => {
+    require ('./Chat.css')
     return ( 
         <div>
             <Menu/>
             <Navegacao/>
+            <div className='containerChat'>
 
-            <h1> Chat </h1>
+                <div className='conversas'>
+                    <ul>
+                        <li>
+                            <img src='#'/>
+                            <p>nome da pessoa</p>
+                        </li>
+                        <li>
+                            <img src='#'/>
+                            <p>nome da pessoa</p>
+                        </li>
+                        <li>
+                            <img src='#'/>
+                            <p>nome da pessoa</p>
+                        </li>
+                        <li>
+                            <img src='#'/>
+                            <p>nome da pessoa</p>
+                        </li>
+                    </ul>
+
+                    
+                </div>
+                <div className='conversa'>
+                    <div className='pessoa'>
+                        <img src='https://i.imgur.com/EnVIUtr.png'/>
+                        <p>Tiffany Silva</p>
+                    </div>
+                    <input type='text' placeholder='Mensagem'>
+                </input>
+                </div>
+                
+
+            </div>
             
         </div>
      );
