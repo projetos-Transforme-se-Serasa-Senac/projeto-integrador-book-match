58228dac7ad7b21e5734afb5c1b056cc48216fc4
Giulia Maia <maiagiulia10@gmail.com>
2022-07-01 14:58:43 -0300

    pedidos

--- src/pages/Pedidos/Pedidos.js
+++ src/pages/Pedidos/Pedidos.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+// import Perfil from '../../Perfil/Perfil'
+
 const Pedidos = () => {
     return(
 
     <div> 
 
+        <h1> Meus Pedidos </h1>
 
+       <a href='Perfil'> Voltar </a>
 
     </div>
 
